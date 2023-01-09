d494d33019fc413b9f910efac62ed84f173a328d
Mustafa karakaş <mustafakarakas61ts862@gmail.com>
2023-01-09 16:34:18 +0300

    PrmSearch upped

---
+++ src/main/java/com/bringtome/ecommerce/model/PrmSearch.java
@@ -0,0 +1,24 @@
+package com.bringtome.ecommerce.model;
+
+
+public class PrmSearch {
+
+    private Integer page;
+    private Integer perPage;
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getPerPage() {
+        return perPage;
+    }
+
+    public void setPerPage(Integer perPage) {
+        this.perPage = perPage;
+    }
+}
